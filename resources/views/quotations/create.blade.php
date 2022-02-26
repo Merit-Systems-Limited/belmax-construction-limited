e979ad141a183787727a7126752bacf7519bd9d6
Junia <51987406+koechjunia001@users.noreply.github.com>
2022-02-27 02:30:09 +0300

    Introduced Quotations

---
+++ resources/views/quotations/create.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.web')
+
+@section('content')
+    <form action="{{ route('quotations.store') }}" method="POST">
+        @csrf
+
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Name:</strong>
+                    <input type="text" name="name" class="form-control" placeholder="Name">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Email:</strong>
+                    <textarea class="form-control" style="height:200px; width:1000px" name="email"
+                        placeholder="Email"></textarea>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>phone:</strong>
+                    <textarea class="form-control" style="height:200px; width:1000px" name="phone"
+                        placeholder="phone"></textarea>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Address:</strong>
+                    <textarea class="form-control" name="address" placeholder="Address"></textarea>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+    </form>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ContactsController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Web\WebController;
+use App\Http\Controllers\ContactsController;
+use App\Http\Controllers\QuotationsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +22,8 @@ Route::get('about-us', [WebController::class, 'about'])->name('about');
 Route::get('contact-us', [ContactsController::class, 'create'])->name('contact.create');
 Route::post('contact-us', [ContactsController::class, 'store'])->name('contact.store');
 
+Route::get('quotations', [QuotationsController::class, 'create'])->name('quotations.create');
+Route::post('quotations', [QuotationsController::class, 'store'])->name('quotations.store');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
